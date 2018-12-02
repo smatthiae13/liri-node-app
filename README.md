54a9195eab5313cf62972a1e1c010ab63a7cd6e6
test <test@tests-MacBook-Pro.local>
2018-12-01 21:06:47 -0600

    readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # liri-node-app
 Language Interpretation and Recognition Interface
+In the Liri Bot app you have fun options to search for information. 
+
+-You can search for where and when your favorite band is going to play with typing into the command line "node liri.js concert-this band name". This uses the API bandsintown.
+
+-The next option is to search for song information through the Spotify API. The search: "node liri.js spotify-this-song songname"
--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 //js
 require("dotenv").config();
 
-var keys = require ("./keys.js");
+var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 var Spotify = require("node-spotify-api");
@@ -16,24 +16,22 @@ var input = process.argv.slice(3).join(" ");
 //axios.get("")
 
 //when users enters in request: "node liri.js concert-this <artist/band name here>"
-function concertThis(artist) {
-  if (!command) {
-      console.log("Band name not found");
-  } else if (command === "concert-this") {
-      var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-  axios.get(URL).then()
-    }
+function bands() {
+    
+    var queryUrl = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp";
 
-
-  
-  
-    // if (command === "concert-this") {
-   //     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-   //     console.log(queryUrl);
-   //     request(queryUrl, function(error, response, body) {
-   //         
-   //     })
-   // }
+    console.log(queryUrl);
+    request(queryUrl, function (error, response, body) {
+        // If the request is successful
+        var pbody = JSON.parse(body);
+        if (!error && response.statusCode === 200) {
+            pbody.forEach(function (element) {
+                console.log("Venue name - " + element.venue.name);
+                console.log("Venue Location - " +  element.venue.city + " , " +  element.venue.region + "  - " + element.venue.country);
+                console.log("Date - " + moment(element.datetime).format("MM/DD/YYYY"));
+            });
+        }
+    });
 }
 
 //when user enters in request: `node liri.js spotify-this-song '<song name here>'`. we pull the info from Spotify
@@ -41,49 +39,57 @@ function spotifyThis() {
     if (name = "") {
         name = "The sign Ace of Base";
     }
-else (command === "spotify-this-song"); {
-    spotify.search({
-        type: "track",
-        query: input,
-        
-    }),
-function (err, data) {
-    if (err) {
-        console.log("error occurred: " + err);
-        return;
+    else (command === "spotify-this-song"); {
+        spotify.search({
+            type: "track",
+            query: input,
+        }),
+            function (err, data) {
+                if (err) {
+                    console.log("error occurred: " + err);
+                    return;
+                }
+                console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                console.log("artists name: ", + data.tracks.items[0].album.artists[0].name);
+                console.log("Song name: ", + data.tracks.items[0].name);
+                console.log("A preview link of the song from Spotify: ", + data.tracks.items[0].preview_url);
+                console.log("The album the song is from: " + data.tracks.items[0].album.name);
+                console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            }
     }
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
-console.log("artists name: ", + data.tracks.items[0].album.artists[0].name);
-console.log("Song name: ", + data.tracks.items[0].name);
-console.log("A preview link of the song from Spotify: ", + data.tracks.items[0].preview_url);
-console.log("The album the song is from: " + data.tracks.items[0].album.name);
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-}
-}
 }
 function movieThis() {
     // This line is just to help us debug against the actual URL.
     if (name === "") {
-      name = "Mr. Nobody";
+        name = "Mr. Nobody";
     }
-  
+
     var queryUrl =
-      "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&tomatoes=true&apikey=trilogy";
-  
-    request(queryUrl, function(error, response, body) {
-      // If the request is successful
-      if (!error && response.statusCode === 200) {
-        // Parse the body of the site and recover just the imdbRating
-        var pbody = JSON.parse(body);
-        console.log("Title - " + pbody.Title);
-        console.log("Release Year: - " + pbody.Year);
-        console.log("IMDB Rating - " + pbody.imdbRating);
-        console.log("Rotten Tomatoes Rating - " + pbody.tomatoRating);
-        console.log("Country where the movie was produced - " + pbody.Country);
-        console.log("Language - " + pbody.Language);
-        console.log("Plot - " + pbody.Plot);
-        console.log("Actors - " + pbody.Actors);
-      }
+        "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            // Parse the body of the site and recover just the imdbRating
+            var pbody = JSON.parse(body);
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~");
+            console.log("Title - " + pbody.Title);
+            console.log("Release Year: - " + pbody.Year);
+            console.log("IMDB Rating - " + pbody.imdbRating);
+            console.log("Rotten Tomatoes Rating - " + pbody.tomatoRating);
+            console.log("Country where the movie was produced - " + pbody.Country);
+            console.log("Language - " + pbody.Language);
+            console.log("Plot - " + pbody.Plot);
+            console.log("Actors - " + pbody.Actors);
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
     });
-  }
+}
+function doWhat() {
+    if (command === " do-what-it-says"){
+    fs.readFile("./random.txt", "utf-8", function(error, data){
+if (error) throw error;
+ });
+}
+}
 fs.appendFile("random.txt", )
